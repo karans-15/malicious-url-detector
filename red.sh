2447175c5fd7039ac66863515bac298f6e9ccd57
Karan Shah <karannshah00@gmail.com>
2021-07-07 09:39:54 +0530

    Delete red.sh

--- red.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
- echo
- for domain in $@; do
- echo --------------------
- echo $domain
- echo --------------------
- curl -sILk $domain | egrep 'HTTP|Loc' | sed 's/Loc/ -> Loc/g'
- echo
- done
